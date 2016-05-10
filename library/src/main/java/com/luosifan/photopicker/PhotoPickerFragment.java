01c10a7ea3e6ddc8adb5bd478e07a4a6de42a990
wzfu <99050625@qq.com>
2016-05-10 20:08:27 +0800

    2.0

---
+++ library/src/main/java/com/luosifan/photopicker/PhotoPickerFragment.java
@@ -0,0 +1,10 @@
+package com.luosifan.photopicker;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * 照片选择页面
+ * Created by wzfu on 2016/5/10.
+ */
+public class PhotoPickerFragment extends Fragment{
+}
---
+++ library/src/main/java/com/luosifan/photopicker/Utils.java
@@ -0,0 +1,7 @@
+package com.luosifan.photopicker;
+
+/**
+ * Created by wzfu on 2016/5/10.
+ */
+public class Utils {
+}
