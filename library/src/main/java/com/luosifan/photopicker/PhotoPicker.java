610e1d357afc4660fd371e04f66e42d233bbb46d
wzfu <99050625@qq.com>
2016-05-16 00:14:03 +0800

    initialize

--- library/src/main/java/com/luosifan/photopicker/PhotoPicker.java
+++ library/src/main/java/com/luosifan/photopicker/PhotoPicker.java
@@ -13,16 +13,18 @@ public class PhotoPicker {
 
     public static PhotoPicker mSingleton = null;
     public final Activity aty;
+    public final ImageLoader imageLoader;
 
-    public PhotoPicker(Activity context) {
+    public PhotoPicker(Activity context, ImageLoader imageLoader) {
         this.aty = context;
+        this.imageLoader = imageLoader;
     }
 
-    public static PhotoPicker with(Activity context){
+    public static PhotoPicker with(Activity context, ImageLoader imageLoader){
         if(mSingleton == null){
             synchronized (PhotoPicker.class){
                 if(mSingleton == null){
-                    mSingleton = new Contractor(context).build();
+                    mSingleton = new Contractor(context, imageLoader).build();
                 }
             }
         }
@@ -33,18 +35,29 @@ public class PhotoPicker {
         return new Load();
     }
 
+    public ImageLoader getImageLoader(){
+        return imageLoader;
+    }
+
     private static class Contractor {
         private final Activity mContext;
+        private final ImageLoader imageLoader;
 
-        public Contractor(Activity context) {
+        public Contractor(Activity context, ImageLoader imageLoader) {
             if (context == null) {
                 throw new IllegalArgumentException("Context must not be null.");
             }
+            if(imageLoader == null){
+                throw new IllegalArgumentException("ImageLoader must not be null.");
+            }
             this.mContext = context;
+            this.imageLoader = imageLoader;
         }
 
         public PhotoPicker build() {
-            return new PhotoPicker(mContext);
+            return new PhotoPicker(mContext, imageLoader);
         }
     }
+
+
 }
--- library/src/main/java/com/luosifan/photopicker/PhotoPickerFragment.java
+++ library/src/main/java/com/luosifan/photopicker/PhotoPickerFragment.java
@@ -1,10 +1,63 @@
 package com.luosifan.photopicker;
 
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * 照片选择页面
  * Created by wzfu on 2016/5/10.
  */
 public class PhotoPickerFragment extends Fragment{
+
+    // 不同loader定义
+    private static final int LOADER_ALL = 0;
+    private static final int LOADER_CATEGORY = 1;
+    // 请求加载系统照相机
+    private static final int REQUEST_CAMERA = 100;
+
+    public static PhotoPickerFragment newInstance(){
+        PhotoPickerFragment fragment = new PhotoPickerFragment();
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.photo_picker_fragment, container, false);
+        return view;
+    }
+
+    private LoaderManager.LoaderCallbacks<Cursor> mLoaderCallback = new LoaderManager.LoaderCallbacks<Cursor>() {
+
+        private final String[] IMAGE_PROJECTION = {
+                MediaStore.Images.Media.DATA,
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.DATE_ADDED,
+                MediaStore.Images.Media.MIME_TYPE,
+                MediaStore.Images.Media.SIZE,
+                MediaStore.Images.Media._ID };
+
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            return null;
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+
+        }
+    };
 }
