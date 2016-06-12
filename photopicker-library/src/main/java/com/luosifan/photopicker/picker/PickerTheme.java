d4903043da19fcee2b5b9947ccc1434920070d79
wzfu <99050625@qq.com>
2016-06-12 23:44:41 +0800

    initialize

--- photopicker-library/src/main/java/com/luosifan/photopicker/picker/PickerTheme.java
+++
@@ -1,7 +0,0 @@
-package com.luosifan.photopicker.picker;
-
-/**
- * Created by wzfu on 16/5/21.
- */
-public class PickerTheme {
-}
--- photopicker-library/src/main/java/com/luosifan/photopicker/picker/SelectMode.java
+++
@@ -1,9 +0,0 @@
-package com.luosifan.photopicker.picker;
-
-/**
- * Created by wzfu on 16/5/21.
- */
-public enum SelectMode {
-    SINGLE,
-    MULTI
-}
